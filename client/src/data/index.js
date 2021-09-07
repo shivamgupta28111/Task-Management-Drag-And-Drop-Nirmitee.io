a4e74c8379cabb8380569f83741496e264849d59
Tube light Coder <70341160+shivamgupta28111@users.noreply.github.com>
2021-09-07 16:19:05 +0530

    Add files via upload

---
+++ client/src/data/index.js
@@ -0,0 +1,46 @@
+const data = [{
+    id: 1,
+    icon: "⭕️",
+    status: "open",
+    title: "Dental Cleaning Appointment",
+    content: "Teeth cleaning"
+}, {
+    id: 2,
+    icon: "⭕️",
+    status: "open",
+    title: "Wisdom teeth Removal",
+    content: "Appointment"
+}, {
+    id: 3,
+    icon: "⭕️",
+    status: "open",
+    title: "Root Canal Appointment",
+    content: "operation"
+}, {
+    id: 4,
+    icon: "⭕️",
+    status: "open",
+    title: "Scaling",
+    content: "consultation"
+}];
+
+const statuses = [{
+    status: "open",
+    icon: "⭕️",
+    color: "#EB5A46"
+}, {
+    status: "in progress",
+    icon: "🔆️",
+    color: "#00C2E0"
+}, {
+    status: "in review",
+    icon: "📝",
+    color: "#C377E0"
+}, {
+    status: "done",
+    icon: "✅",
+    color: "#3981DE"
+}];
+
+
+export { data, statuses };
